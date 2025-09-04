5ce4b542f1fcb9d53420cc00377c286fd83ae463
Yashwant Sahoo <yashwantsahoo05@gmail.com>
2025-09-04 23:24:14 +0530

    counterapi

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -6,33 +6,41 @@ const Footer = () => {
   const [visitors, setVisitors] = useState(0);
 
   useEffect(() => {
-  const fetchVisitors = async () => {
-    try {
-      const response = await axios.get(
-        "https://counterapi.dev/v2/workspaces/yash-portfolio/counters/unique-visitors-ys10/increment"
-      );
-      setVisitors(response.data.count ?? response.data.value);
-    } catch (error) {
-      console.error("Error fetching visitor count:", error);
-    }
-  };
+    const fetchUniqueVisitors = async () => {
+      try {
+        const response = await axios.get(
+          "https://counterapi.dev/v2/workspaces/yash-portfolio/counters/unique-visitors-ys10/increment"
+        );
+        setVisitors(response.data.value ?? response.data.count);
+      } catch (error) {
+        console.error("Error fetching visitor count:", error);
+      }
+    };
 
-  fetchVisitors();
-}, []);
-
+    fetchUniqueVisitors();
+  }, []);
 
   return (
     <section className="flex flex-wrap items-center justify-between gap-5 pb-3 text-sm text-neutral-400 c-space">
       <div className="mb-4 bg-gradient-to-r from-transparent via-neutral-700 to-transparent h-[1px] w-full" />
 
       <div className="flex gap-4 divide-x divide-neutral-600">
-  <a href="/terms.html" className="pr-4 hover:text-white">Terms & Conditions</a>
-  <a href="/privacy.html" className="pl-4 hover:text-white">Privacy Policy</a>
-</div>
+        <a href="/terms.html" className="pr-4 hover:text-white">
+          Terms & Conditions
+        </a>
+        <a href="/privacy.html" className="pl-4 hover:text-white">
+          Privacy Policy
+        </a>
+      </div>
 
       <div className="flex items-center gap-3">
         {mySocials.map((social, index) => (
-          <a href={social.href} key={index} target="_blank" rel="noopener noreferrer">
+          <a
+            href={social.href}
+            key={index}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img src={social.icon} className="w-5 h-5" alt={social.name} />
           </a>
         ))}
